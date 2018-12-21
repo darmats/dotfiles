d11faa31688edef19168da7b7f12495b0e45f73c
Hiroki Matsuda <h.matsu0330@gmail.com>
2018-12-21 13:05:23 +0900

    wip

--- install.py
+++ install.py
@@ -6,9 +6,9 @@ now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
 
 def exists(command):
-    f_null = open(os.devnull, "w")
-    ret = call(["command", "-v", command], stdout=f_null, stderr=f_null)
-    return ret == 0
+    with open(os.devnull, "w") as f:
+        ret = call(["command", "-v", command], stdout=f, stderr=f)
+        return ret == 0
 
 
 if not exists("brew"):
